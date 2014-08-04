829a1e967ef3e7a173ae8184aa65963b2700d3d0
Zhang Shuai <satan.student@gmail.com>
2014-08-04 15:24:20 +0800

    Finish chapter 1.

--- chap1-unittest.rkt
+++ chap1-unittest.rkt
@@ -45,3 +45,66 @@
               '(bar
                 (bar 2 (foo 2 4))
                 (biz 8 10)))
+
+; Exercise 1.33 [**]
+(check-equal? (mark-leaves-with-red-depth
+               (interior-node 'red
+                              (interior-node 'bar
+                                             (leaf 26)
+                                             (leaf 12))
+                              (interior-node 'red
+                                             (leaf 11)
+                                             (interior-node 'quux
+                                                            (leaf 117)
+                                                            (leaf 14)))))
+              '(red
+                (bar 1 1)
+                (red 2 (quux 2 2))))
+
+; Exercise 1.34 [***]
+(check-equal? (path 17 '(14 (7 () (12 () ()))
+                            (26 (20 (17 () ())
+                                    ())
+                                (31 () ()))))
+              '(right left left))
+
+; Exercise 1.35 [***]
+(check-equal? (number-leaves
+               (interior-node
+                'foo
+                (interior-node
+                 'bar
+                 (leaf 26)
+                 (leaf 12))
+                (interior-node
+                 'baz
+                 (leaf 11)
+                 (interior-node
+                  'quux
+                  (leaf 117)
+                  (leaf 14)))))
+              '(foo
+                (bar 0 1)
+                (baz 2 (quux 3 4))))
+
+; Exercise 1.36 [***]
+(define number-elements-from
+  (lambda (lst n)
+    (if (null? lst)
+        '()
+        (cons
+         (list n (car lst))
+         (number-elements-from (cdr lst) (+ n 1))))))
+
+(define number-elements
+  (lambda (lst)
+    (number-elements-from lst 0)))
+
+(define number-elements.
+  (lambda (lst)
+    (if (null? lst)
+        '()
+        (g (list 0 (car lst)) (number-elements. (cdr lst))))))
+
+(check-equal? (number-elements. '(a b c d))
+              (number-elements '(a b c d)))
--- chap1.rkt
+++ chap1.rkt
@@ -5,12 +5,12 @@
 
 (define (foldl f init lst)
   (cond
-    [(eq? lst '()) init]
+    [(null? lst) init]
     [else (foldl f (f (car lst) init) (cdr lst))]))
 
 (define (foldr f init lst)
   (cond
-    [(eq? lst '()) init]
+    [(null? lst) init]
     [else (f (car lst) (foldr f init (cdr lst)))]))
 
 (define (duple n x)
@@ -59,11 +59,75 @@
 (define (double-tree t)
   (let ([double (lambda (n) (* 2 n))])
     (cond
-      [(leaf? t) (double (contents-of t))]
+      [(leaf? t) (leaf (double (contents-of t)))]
       [else (interior-node
              (contents-of t)
              (double-tree (lson t))
              (double-tree (rson t)))])))
+
+(define (mark-leaves-with-red-depth t)
+  (let loop ([counter 0] [t t])
+    (cond
+      [(leaf? t) (leaf counter)]
+      [else (let* ([s (contents-of t)]
+                   [c (if (eq? s 'red)
+                          (add1 counter)
+                          counter)])
+              (interior-node
+               s
+               (loop c (lson t))
+               (loop c (rson t))))])))
+
+(define (path n bst)
+  (let ([s (contents-of bst)])
+    (cond
+      [(= n s) '()]
+      [(< n s) (cons 'left (path n (lson bst)))]
+      [(> n s) (cons 'right (path n (rson bst)))])))
+
+(define (number-leaves bt)
+  (let ([c 0])
+    (let loop ([bt bt])
+      (cond
+        [(leaf? bt) (let ([c0 c])
+                      (begin
+                        (set! c (add1 c))
+                        (leaf c0)))]
+        [else (interior-node
+               (contents-of bt)
+               (loop (lson bt))
+               (loop (rson bt)))]))))
+
+#|
+; CPS style loop without changing any values
+(define (number-leaves bt)
+  (let loop ([bt bt]
+             [c 0]
+             [cont (lambda (x c) x)])
+    (cond
+      [(leaf? bt) (cont (leaf c) (add1 c))]
+      [else
+       (loop (lson bt)
+             c
+             (lambda (l c)
+               (loop (rson bt)
+                     c
+                     (lambda (r c)
+                       (cont (interior-node (contents-of bt) l r)
+                             c)))))])))
+|#
+
+(define (g hd tl)
+  (cons
+   hd
+   (if (null? tl)
+      '()
+      (let add-from-n-to-elements ([n (add1 (car hd))]
+                                   [lst tl])
+        (if (null? lst)
+            '()
+            (cons (cons n (cdar lst))
+                  (add-from-n-to-elements (add1 n) (cdr lst))))))))
 
 (provide duple
          invert
@@ -76,4 +140,8 @@
          lson
          rson
          contents-of)
-(provide double-tree)
+(provide double-tree
+         mark-leaves-with-red-depth
+         path
+         number-leaves
+         g)
