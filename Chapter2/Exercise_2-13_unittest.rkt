8654543588385f6ce9f9746a1408e55b540cc221
Zhang Shuai <satan.student@gmail.com>
2014-08-06 16:34:15 +0800

    Use check-true or check-false instead of explicit check-eq? #t or #f.

--- Chapter2/Exercise_2-13_unittest.rkt
+++ Chapter2/Exercise_2-13_unittest.rkt
@@ -9,8 +9,8 @@
                                       (extend-env 'b 2
                                                   (empty-env))))))
 
-(check-equal? (empty-env? (empty-env)) #t)
-(check-equal? (empty-env? env) #f)
+(check-true   (empty-env? (empty-env)))
+(check-false  (empty-env? env))
 (check-equal? (apply-env env 'a) 1)
 (check-equal? (apply-env env 'b) 2)
 (check-equal? (apply-env env 'c) 3)
