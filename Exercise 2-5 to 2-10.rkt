183cb09d6ebadc5eae783db9243b97c9f860abdc
Zhang Shuai <satan.student@gmail.com>
2014-08-06 14:40:20 +0800

    Finish exercise 2.5 to 2.10

---
+++ Exercise 2-5 to 2-10.rkt
@@ -0,0 +1,38 @@
+#lang eopl
+
+(define report-no-binding-found
+  (lambda (search-var)
+    (eopl:error 'apply-env "No binding for ~s" search-var)))
+
+; Exercise 2.5 [*]
+(define empty-env (lambda () '()))
+(define extend-env
+  (lambda (var val env)
+    (cons (cons var val) env)))
+(define apply-env
+  (lambda (env search-var)
+    (let ([a-pair (assoc search-var env)])
+      (if (eq? #f a-pair)
+          (report-no-binding-found search-var)
+          (cdr a-pair)))))
+
+; Exercise 2.8 [*]
+(define (empty-env? env) (null? env))
+
+; Exercise 2.9 [*]
+(define (has-binding? env search-var)
+  (pair? (assoc search-var env)))
+
+; Exercise 2.10 [*]
+(define (extend-env* vars vals env)
+  (cond [(null? vars)
+         (cond [(null? vals) env]
+               [else
+                eopl:error 'extend-env* "vars & vals have different length."])]
+        [else
+         (cond [(null? vals)
+                eopl:error 'extend-env* "vars & vals have different length."]
+               [else
+                (extend-env* (cdr vars)
+                             (cdr vals)
+                             (extend-env (car vars) (car vals) env))])]))
